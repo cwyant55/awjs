131090ec63de7e78a946da59175703b15df89a0b
cwyant55 <curtiswyant@gmail.com>
2016-10-13 13:01:19 -0700

    Upload working.

--- js/awjs.js
+++ js/awjs.js
@@ -166,3 +166,44 @@ awApp.controller("searchController", function($scope,$http){
     };
 	
 });
+
+// Dropzonejs stuff
+$(document).ready(function(){
+	
+Dropzone.autoDiscover = false; // keep this line if you have multiple dropzones in the same page
+
+var uploadForm = new Dropzone('#uploadform', {
+
+//	acceptedFiles: "text/csv",
+	autoProcessQueue: false,
+	url: './php/upload.php',
+//	maxFiles: 5, // Number of files at a time
+	maxFilesize: 10, //in MB
+	maxfilesexceeded: function(file)
+		{ alert('You have uploaded more than 1 Image. Only the first file will be uploaded!'); },
+	
+	success: function (response) {
+	var x = JSON.parse(response.xhr.responseText);
+	this.removeAllFiles(); // This removes all files after upload to reset dropzone for next upload
+	console.log(response); // Just to return the JSON to the console.
+		if (response.status == 'success') {
+		var msg = 'File ' + response.name + ' sucessfully uploaded.';
+		$('.alert-success > p').html(msg);
+        $('.alert-success').show();
+        $('.alert-success').delay(5000).slideUp(function(){
+            $('.alert-success > p').html('');
+        });
+	}
+	},
+	addRemoveLinks: true,
+	removedfile: function(file) {
+	var _ref; // Remove file on clicking the 'Remove file' button
+	return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
+}
+});
+
+$('#add').click(function(){
+	uploadForm.processQueue();
+});
+
+});
--- php/upload.php
+++ php/upload.php
@@ -1,62 +1,35 @@
 <?php
-$target_dir = "/var/www/public/awjs/upload/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+$ds          = DIRECTORY_SEPARATOR;  //1
+ 
+$storeFolder = '/var/www/public/awjs/upload';   //2
+ 
+if (!empty($_FILES)) {
+     
+    $tempFile = $_FILES['file']['tmp_name'];          //3             
+      
+#    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+    $targetPath = $storeFolder . $ds;  //4
+     
+    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+ 
+    move_uploaded_file($tempFile,$targetFile); //6
+     
 }
 
 
 // index and commit file to Solr
+$ch = curl_init("http://localhost:8983/solr/awjs/update?commit=true");
 
-#$ch = curl_init("http://localhost:8983/solr/awjs/update?commit=true");
+$data = file_get_contents($targetFile);
 
-#$data = file_get_contents('/var/www/public/awjs/upload/SampleLog.csv');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/csv'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-#curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-#curl_setopt($ch, CURLOPT_POST, TRUE);
-#curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/csv'));
-#curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-#$response = curl_exec($ch);
+$response = curl_exec($ch);
 
 // get response status
-#$xml = simplexml_load_string($response);
-#echo $xml->lst->int;
+$xml = simplexml_load_string($response);
+echo $xml->lst->int;
 ?>
